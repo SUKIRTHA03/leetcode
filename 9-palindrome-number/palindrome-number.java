77d1716c2f4814129134dbebf1d4976b9d210ea4
SUKIRTHA RAJA <sukirthar.cs24@bitsathy.ac.in>
2026-09-19 09:09:07 +0530

    Time: 4 ms (100.00%) | Memory: 46.2 MB (16.30%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int org=x;
+        int rev=0;
+        if(x==0) return true;
+        while(x>0){
+            int digits=x%10;
+            rev=rev*10+digits;
+            x/=10;
+        }
+        if(org==rev) return true;
+        else return false;
+    }
+}
