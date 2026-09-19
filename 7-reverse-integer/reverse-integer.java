087aacc37e0ebd953c0d0dfefb5820aa4f7ff663
SUKIRTHA RAJA <sukirthar.cs24@bitsathy.ac.in>
2026-09-19 09:21:28 +0530

    Time: 1 ms (99.96%) | Memory: 42.8 MB (26.72%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long rev=0;
+        if(x==0) return 0;
+        while(x!=0){
+            int digits=x%10;
+            rev=rev*10+digits;
+            x/=10;
+        }
+        if(rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE) return 0;
+        return (int)rev;
+    }
+}
